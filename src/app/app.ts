ce2c872802f2e3021b00b76523840ca2e6a6312a
jimi-chhatrala <jimichhatrala8488@gmail.com>
2026-06-07 17:51:14 +0530

    1-interpolation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('angular-tutorial');
+  protected readonly title = signal('angular-tutorial signal');
+  name: string = 'Angular Tutorial';
+  isAdmin: boolean = true;
+  count: number = 5
+  price: number = 199.99;
+  isLogin: boolean = true;
+
+  getUser() {
+    return 'Enjoy Angular Tutorial';
+  }
 }
