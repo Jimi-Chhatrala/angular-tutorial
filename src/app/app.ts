4c83be6c7396d75f8e20fb32adb2e3cb54241af8
jimi-chhatrala <jimichhatrala8488@gmail.com>
2026-06-28 18:09:02 +0530

    18-effects

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,53 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('angular-tutorial');
+
+  count = signal(0);
+
+  isDarkMode = signal(false);
+
+  message = signal('');
+
+  constructor() {
+    effect(() => {
+      console.log(`Count value:`, this.count());
+    });
+
+    effect(() => {
+      if (this.isDarkMode()) {
+        document.body.style.backgroundColor = 'black';
+        document.body.style.color = 'white';
+      } else {
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+      }
+    });
+
+    effect(() => {
+      if (this.message()) {
+        setTimeout(() => {
+          this.message.set('');
+        }, 3000);
+      }
+    });
+  }
+
+  showMessage() {
+    this.message.set('Setting this message on button click.');
+  }
+
+  toggle() {
+    this.isDarkMode.update((mode) => !mode);
+  }
+
+  increment() {
+    this.count.update((c) => c + 1);
+  }
 }
