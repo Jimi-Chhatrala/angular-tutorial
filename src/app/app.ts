08c45292a57f3b38ef8876822207988d85041d16
jimi-chhatrala <jimichhatrala8488@gmail.com>
2026-06-28 18:36:49 +0530

    19-built-in-pipes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('angular-tutorial');
+
+  firstName: string = 'john';
+  lastName: string = 'doe';
+  fullName: string = this.firstName + ' ' + this.lastName;
+
+  todayDate = new Date();
+
+  amount = 34500;
+
+  percentage = 0.35;
+
+  userData = {
+    name: this.firstName + ' ' + this.lastName,
+    age: 25,
+  };
 }
