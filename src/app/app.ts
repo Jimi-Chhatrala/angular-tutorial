7a5ac0e574a74d37595b32313d38bd82baaa0040
jimi-chhatrala <jimichhatrala8488@gmail.com>
2026-07-11 17:19:22 +0530

    23-todo-app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,35 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('angular-tutorial');
+
+  newTodo = signal('');
+
+  todos = signal<{ text: string; done: boolean }[]>([]);
+
+  addTodo() {
+    const text = this.newTodo().trim();
+
+    if (!text) return;
+
+    this.todos.update((list) => [...list, { text, done: false }]);
+
+    this.newTodo.set('');
+  }
+
+  toggleTodo(index: number) {
+    this.todos.update((list) =>
+      list.map((item, i) => (i === index ? { ...item, done: !item.done } : item)),
+    );
+  }
+
+  deleteTodo(index: number) {
+    this.todos.update((list) => list.filter((_, i) => i !== index));
+  }
 }
