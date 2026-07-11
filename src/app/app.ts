aed30c5d723140efe958601cff117426dae20f3e
jimi-chhatrala <jimichhatrala8488@gmail.com>
2026-07-11 16:02:20 +0530

    22-two-way-binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,29 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('angular-tutorial');
+
+  name: string = 'John';
+
+  username = signal('Hello');
+
+  userData = signal<{ name: string; age: number }>({
+    name: 'World',
+    age: 25,
+  });
+
+  updateName(value: string) {
+    this.userData.update((user) => ({ ...user, name: value }));
+  }
+
+  updateAge(increaseBy: string | number) {
+    this.userData.update((user) => ({ ...user, age: user.age + Number(increaseBy) }));
+  }
 }
