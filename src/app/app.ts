28d03eab3657a94590ae7edae0bdc42b99138fd2
jimi-chhatrala <jimichhatrala8488@gmail.com>
2026-07-20 23:51:49 +0530

    26-store-with-signal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CounterStore } from './core/store/counter';
+import { UserStore } from './core/store/user';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('angular-tutorial');
+
+  constructor(
+    public counterStore: CounterStore,
+    public userStore: UserStore,
+  ) {}
 }
