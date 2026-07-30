e10bca590c1d5b5a610191676e11079d9994bda6
jimi-chhatrala <jimichhatrala8488@gmail.com>
2026-07-30 23:31:09 +0530

    33-lazy-loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: Home,
+  },
+  {
+    path: 'admin',
+    loadComponent: () => import('./admin/admin').then((m) => m.Admin),
+  },
+];
